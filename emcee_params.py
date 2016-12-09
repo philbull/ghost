1e9f17738fd6b90492d7c0117cb21ebbcbfba07f
Phil Bull <philbull@gmail.com>
2016-12-09 08:37:18 -0800

    Allow multi-band optical fits, improve extinction handling, implement data cuts and better priors

--- emcee_params.py
+++ emcee_params.py
@@ -5,44 +5,65 @@ Metropolis-Hastings MCMC to find best-fit parameters for the galaxy model.
 $ mpirun --bind-to none -n 1 python emcee_params.py
 """
 import numpy as np
-#import pylab as P
 import copy, sys, time
 import emcee
 #from emcee.utils import MPIPool
 import galaxy_model as g
 import likelihoods as like
 
-np.random.seed(12)
+np.random.seed(18) #17 #12
 
 NTHREADS = 64
-NSAMPLES = 1000
-BAND = 'z'
-CHAIN_FILE = "chainx_opt_rad.dat"
-nwalkers = 64 # nwalkers needs to be at least twice ndim (11*2 here)
+NSAMPLES = 2000 #200 #1000
+BANDS = ['u', 'g', 'r', 'i', 'z']
+#BANDS = ['g', 'z']
+#CHAIN_FILE = "chainx_opt_rad.dat"
+#CHAIN_FILE = "chainx_opt_rad_atten.dat"
+#CHAIN_FILE = "chain_new_gzrad_atten.dat"
+#CHAIN_FILE = "chain_new_gzrad_smf_atten.dat"
+CHAIN_FILE = "chain_gzrad_atten_3.dat"
+nwalkers = 128 #64 # nwalkers needs to be at least twice ndim (11*2 here)
+
+MSTAR_MIN = None
+HUBBLE = 0.67
+OMEGA_M = 0.32
 
 # Set initial model parameters
 #params0 = copy.copy(g.default_params)
-
-# Hand-calibrated parameters (good for radio/opt, worse for SMF)
-params0 = {'sfr_sfms_sigma': 0.4, 'mass_mstar_min': 6.0, 'opt_bands': ['u', 'g', 'r', 'i', 'z'], 'fpass_alpha0': 10.8, 'fpass_alpha1': 0.5, 'sfr_pass_pthres': 0.05, 'fpass_zeta': -0.3, 'ms_cen_nu': -0.72, 'mass_mstar_max': 14.0, 'opt_pdf_sigma': [0.28122634653850337, 0.25232918833346546, 0.2468073941298409, 0.25273681573440887, 0.2724513351999828], 'sfr_pass_beta': 0.37, 'mass_mhalo_max': 16.0, 'ms_cen_sigma1': 0.05, 'opt_offset': [-27.3176081, -25.9445857, -25.2836569, -24.981922, -24.8096689], 'opt_cross_amp': [-3.61194266, -2.70027825, -2.01635745, -1.72575487, -1.56393268], 'opt_mstar_beta': -0.000242854097, 'mhi_omegaHI': 0.0001, 'ms_cen_mu': 0.019, 'nsamp_mstar': 200, 'ms_cen_norm': 0.02, 'opt_cross_beta': -0.262636097, 'ms_cen_gamma0': 0.65, 'ms_cen_gamma1': -0.26, 'mass_mhalo_min': 5.0, 'ms_cen_xi': 4.25, 'opt_cross_gamma': 0.290177366, 'opt_mstar_amp': 3876.55879, 'ms_cen_logM2': 11.8, 'ms_cen_logM1': 12.1, 'sfr_pass_gamma': 1.9, 'sfr_min': -5.0, 'sfr_pass_mshift': 0.03, 'ms_cen_sigmainf': 0.16, 'sfr_pass_sfrmin': 1e-07, 'sfr_sfms_gamma': 1.9, 'sfr_pass_mscale': 1.0, 'sfr_sfms_mscale': 1.0, 'sfr_sfms_beta': 0.9, 'nsamp_mhalo': 200, 'sfr_sfms_alpha1': 1.07, 'sfr_sfms_alpha0': 0.005, 'opt_pdf_mean': [-0.06587481168919591, -0.053777765775930214, -0.01854712885192855, -0.008538656095465969, -0.008732300503716532], 'nsamp_sfr': 200, 'sfr_pass_sigma': 0.4, 'ms_cen_beta0': 1.4, 'ms_cen_beta1': 0.17, 'opt_mstar_c': -1.0108709, 'sfr_max': 4.0, 'fpass_beta': -1.1, 'sfr_pass_type': 'shifted sfms', 'sfr_pass_alpha1': 0.0, 'sfr_pass_alpha0': -5.0, 'mhi_vc1': 200.0, 'mhi_vc0': 50.0}
+#params0 = {'sfr_sfms_sigma': 0.38750005196600001, 'mass_mstar_min': 6.0, 'opt_bands': ['u', 'g', 'r', 'i', 'z'], 'fpass_alpha0': 10.8205970498, 'fpass_alpha1': 0.5, 'sfr_pass_pthres': 0.05, 'fpass_zeta': -1.5353087936700001, 'ms_cen_nu': -0.72, 'mass_mstar_max': 14.0, 'opt_pdf_sigma': [0.28122634653850337, 0.25232918833346546, 0.2468073941298409, 0.25273681573440887, 0.2724513351999828], 'extinction_alpha': -7.57886, 'extinction_beta': 5.4223162433100001, 'sfr_pass_beta': 0.37, 'extinction_diskfac': 13.516079320399999, 'mass_mhalo_max': 16.0, 'ms_cen_sigma1': 0.60802361084000001, 'opt_offset': [-27.3176081, -25.9445857, -25.2836569, -24.981922, -24.8096689], 'ms_cen_logM1': 12.489042828500001, 'mhi_omegaHI': 0.0001, 'ms_cen_mu': 0.019, 'nsamp_mstar': 300, 'ms_cen_norm': 0.015799159819799999, 'nsamp_mhalo': 200, 'opt_cross_beta': -0.262636097, 'ms_cen_gamma0': 0.54792407909100005, 'ms_cen_gamma1': -0.26, 'extinction_amp': 0.29403960120099998, 'mass_mhalo_min': 7.0, 'ms_cen_xi': 4.25, 'opt_cross_gamma': 0.290177366, 'opt_mstar_amp': 3876.55879, 'ms_cen_logM2': 11.8, 'opt_mstar_beta': -0.000242854097, 'sfr_pass_type': 'shifted sfms', 'sfr_min': -9.0, 'sfr_pass_mshift': 0.0053486148026400003, 'ms_cen_sigmainf': -0.031605451195699998, 'sfr_pass_alpha1': 0.0, 'sfr_sfms_gamma': 1.9, 'nsamp_sfr': 250, 'sfr_sfms_mscale': 1.0, 'sfr_sfms_beta': 1.0910253563500001, 'opt_cross_amp': [-3.61194266, -2.70027825, -2.01635745, -1.72575487, -1.56393268], 'sfr_sfms_alpha1': 1.07, 'sfr_sfms_alpha0': -0.10162165209600001, 'opt_pdf_mean': [-0.06587481168919591, -0.053777765775930214, -0.01854712885192855, -0.008538656095465969, -0.008732300503716532], 'sfr_pass_mscale': 1.0, 'sfr_pass_sigma': 0.012386348129800001, 'ms_cen_beta0': 1.29481129595, 'ms_cen_beta1': 0.17, 'opt_mstar_c': -1.0108709, 'sfr_max': 4.0, 'fpass_beta': -2.7709240453000001, 'sfr_pass_gamma': 1.9, 'sfr_pass_sfrmin': 1e-07, 'sfr_pass_alpha0': -5.0, 'mhi_vc1': 200.0, 'mhi_vc0': 50.0, 'extinction_tau0': 0.452}
+from bestfit import *
+params0 = params_bf
 
 # Load 6dFGS local radio LF data from Mauch & Sadler (astro-ph/0612018)
 L_radio, Phi_radio, errp_radio, errm_radio = \
-                        like.load_mauch_lf(fname="lumfunc_6dfgs.dat", h=0.67)
+               like.load_mauch_lf(fname="../lumfns/lumfunc_6dfgs.dat", h=HUBBLE)
 logPhi_radio = np.log10(Phi_radio)
 
 # Load GAMA local optical LF data from Driver et al. (2012)
-mag_gama, Phi_gama, err_gama = like.load_gama_lf(band=BAND, h=0.67)
+data_gama = []
+for BAND in BANDS:
+    mag_gama, Phi_gama, err_gama = like.load_gama_lf(band=BAND, h=HUBBLE)
+    # FIXME: Apply magnitude cut to avoid incompleteness effects
+    mag_gama = mag_gama[:-3]
+    Phi_gama = Phi_gama[:-3]
+    err_gama = err_gama[:-3]
+    data_gama.append([mag_gama, Phi_gama, err_gama])
 
 # Load SDSS-GALEX stellar mass function
 mstar_sdss, sf_phi_sdss, sf_errp_sdss, sf_errm_sdss, \
-            qu_phi_sdss, qu_errp_sdss, qu_errm_sdss = like.load_sdss_smf(h=0.67)
+            qu_phi_sdss, qu_errp_sdss, qu_errm_sdss \
+                             = like.load_sdss_smf(h=HUBBLE, mstar_min=MSTAR_MIN)
+# Trim low-mass datapoints
+mstar_sdss, sf_phi_sdss, sf_errp_sdss, sf_errm_sdss, \
+qu_phi_sdss, qu_errp_sdss, qu_errm_sdss \
+  = [var[4:] for var in (mstar_sdss, sf_phi_sdss, sf_errp_sdss, sf_errm_sdss,
+                         qu_phi_sdss, qu_errp_sdss, qu_errm_sdss)]
 sf_logphi_sdss = np.log10(sf_phi_sdss)
 qu_logphi_sdss = np.log10(qu_phi_sdss)
 
 # Pre-calculate my halo model
 pkfile = "camb_pk_z0.dat"
-hm = g.HaloModel(pkfile, h=0.67, om=0.32)
+hm = g.HaloModel(pkfile, h=HUBBLE, om=OMEGA_M)
 
 #-------------------------------------------------------------------------------
 
@@ -56,19 +77,20 @@ def radio_lumfn(L, params, z=0.):
     phi = dndlogsfr_sfms #+ dndlogsfr_pass # FIXME: Remove passive galaxies
     return phi
 
-def optical_lumfn(obsmag, band, params, z=0.):
+def optical_lumfn(obsmag, band, params, z=0., atten=True):
     """
     Calculate optical luminosity function in some band, for a given set of 
     model parameters.
     """
-    #intmag = np.linspace()
-    #dndmag_sfms_dust, dndmag_pass_dust = g.optical_mag_fn_dust( 
-    #                                        hm, obsmag, intmag, band=band, 
-    #                                        z=z, params=params )
-    #phi = dndmag_sfms_dust + dndmag_pass_dust
-    
-    dndmag_sfms, dndmag_pass = g.optical_mag_fn( hm, obsmag, band, z=z, 
-                                                 params=params )
+    # Decide whether dust-attenuated optical luminosity fn. should be used
+    if atten and band != 'z': # FIXME: Temporary hack to ignore atten. for z-band
+        # Include dust attenuation correction in the optical
+        dndmag_sfms, dndmag_pass = g.optical_mag_fn_atten( hm, obsmag, band, 
+                                                           z=z, params=params )
+    else:
+        # No dust attenuation in the optical
+        dndmag_sfms, dndmag_pass = g.optical_mag_fn( hm, obsmag, band, z=z, 
+                                                     params=params )
     phi = dndmag_sfms + dndmag_pass
     return phi
 
@@ -134,84 +156,61 @@ def loglike(pvals, pnames, params0):
     if p['sfr_sfms_sigma'] < 0.05: return -np.inf
     if p['sfr_pass_sigma'] > 2.:   return -np.inf
     if p['sfr_pass_sigma'] < 0.01: return -np.inf
-    #if p['sfr_sfms_alpha0'] > 1.:  return -np.inf
-    #if p['sfr_pass_alpha0'] > 2.: return -np.inf
-    #if p['sfr_pass_alpha0'] > 2.: return -np.inf
     if p['sfr_pass_mshift'] > 0.9: return -np.inf
     if p['sfr_pass_mshift'] < 1e-3: return -np.inf
-    
+    if p['ms_cen_sigma1'] < 2e-2: return -np.inf
+    if p['ms_cen_sigmainf'] < 2e-2: return -np.inf
     if p['ms_cen_logM1'] < 11.6: return -np.inf
     if p['ms_cen_logM1'] > 12.5: return -np.inf
-    # Make sure passive and active sequences are mostly disjoint
-    #if p['sfr_pass_mshift'] > 0.9 * 10.**(-p['sfr_pass_sigma']): return -np.inf
     
     # Calculate log-likelihoods for each dataset
     try:
         # Radio luminosity function
         logl_rad = loglike_radio_lf(L_radio, logPhi_radio, errp_radio, 
                                     errm_radio, params=p)
-        # Optical luminosity function
-        logl_opt = loglike_optical_lf(mag_gama, Phi_gama, err_gama, band=BAND, 
-                                      params=p)
+        
+        # Optical luminosity function, per-band
+        logl_opt = []
+        for BAND, data in zip(BANDS, data_gama):
+            mag_gama, Phi_gama, err_gama = data
+            _logl_opt = loglike_optical_lf(mag_gama, Phi_gama, err_gama, 
+                                           band=BAND, params=p)
+            logl_opt.append(_logl_opt)
+            
         # Stellar mass function (star-forming and passive)
-        logl_smf = loglike_smf( mstar_sdss, 
-                                sf_logphi_sdss, sf_errp_sdss, sf_errm_sdss,
-                                qu_logphi_sdss, qu_errp_sdss, qu_errm_sdss,
-                                params=p, z=0.)
-        print logl_rad, logl_opt, logl_smf
-        #print logl_rad, logl_smf
-        #print logl_smf
+        #logl_smf = loglike_smf( mstar_sdss, 
+        #                        sf_logphi_sdss, sf_errp_sdss, sf_errm_sdss,
+        #                        qu_logphi_sdss, qu_errp_sdss, qu_errm_sdss,
+        #                        params=p, z=0.)
+        
+        # Output likelihood report
+        print logl_opt, logl_rad, "||", np.sum(logl_opt) + logl_rad
     except BaseException as e:
-        print e
+        print "Error:", e
         return -np.inf
     
     # Sanity checks
-    logl = logl_rad + logl_opt #+ logl_smf/5. # FIXME: Downweighted SMF
-    #logl = logl_rad + logl_smf / 10.
-    #logl = logl_smf
+    logl = np.sum(logl_opt) + logl_rad #+ logl_smf
     if np.isnan(logl): logl = -np.inf
-    #print logl
     return logl
+
 
 #-------------------------------------------------------------------------------
 
 # Specify parameters to sample and initial values
-#pnames = ['sfr_sfms_alpha0', 'sfr_sfms_beta', 'sfr_sfms_sigma', 
-#          'sfr_pass_alpha0', 'sfr_pass_beta', 'sfr_pass_sigma', 
-#          'ms_cen_norm', 'ms_cen_logM1', 'ms_cen_beta0', 'ms_cen_gamma0',
-#          'ms_cen_sigmainf', 'ms_cen_sigma1',
-#          'fpass_alpha0', 'fpass_beta', 'fpass_zeta',
-#          ]
-#pnames = ['ms_cen_norm', 'ms_cen_logM1', 'ms_cen_beta0', 'ms_cen_gamma0',
-#          'ms_cen_sigmainf', 'ms_cen_sigma1',
-#          'fpass_alpha0', 'fpass_beta', 'fpass_zeta',
-#          ]
-#pnames = ['sfr_sfms_alpha0', 'sfr_sfms_beta', 'sfr_sfms_sigma', 
-#          'sfr_pass_alpha0', 'sfr_pass_beta', 'sfr_pass_sigma', 
-#          'fpass_alpha0', 'fpass_beta', 'fpass_zeta',
-#          ]
-#pnames = ['sfr_sfms_alpha0', 'sfr_sfms_beta', 'sfr_sfms_sigma', 
-#          'sfr_pass_mshift', 'sfr_pass_sigma', 
-#          'ms_cen_norm', 'ms_cen_logM1', 'ms_cen_beta0', 'ms_cen_gamma0',
-#          'ms_cen_sigmainf', 'ms_cen_sigma1',
-#          'fpass_alpha0', 'fpass_beta', 'fpass_zeta',
-#          ]
-# SMF-params only
-#pnames = ['fpass_alpha0', 'fpass_beta', 'fpass_zeta',
-#          'ms_cen_beta0', 'ms_cen_gamma0', 'ms_cen_logM1',
-#          'ms_cen_norm', 'ms_cen_sigma1', 'ms_cen_sigmainf',
-#          'ms_cen_logM2', 'ms_cen_xi' ]
-
-# Most SMF params, all SFR params
 pnames = ['sfr_sfms_alpha0', 'sfr_sfms_beta', 'sfr_sfms_sigma', 
           'sfr_pass_mshift', 'sfr_pass_sigma', 
           'ms_cen_norm', 'ms_cen_logM1', 'ms_cen_beta0', 'ms_cen_gamma0',
           'ms_cen_sigmainf', 'ms_cen_sigma1',
           'fpass_alpha0', 'fpass_beta', 'fpass_zeta',
+          'extinction_tau0', 'extinction_beta', 'extinction_diskfac', 
           ]
-
+          
 p0 = np.array([params0[pp] for pp in pnames])
 ndim = p0.size
+
+logl0 = loglike(p0, pnames, params0)
+#exit()
 
 # Get random initial positions for walkers (best-fit values x some O(1) factor)
 p0 = np.outer(np.ones(nwalkers), p0)
